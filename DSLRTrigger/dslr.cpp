779ccd2603b6c63357082a3c144eaea975dedb95
Andrew <catblacka@hotmail.com>
2013-04-15 15:27:08 -0400

    Added the C++ files

---
+++ DSLRTrigger/dslr.cpp
@@ -0,0 +1,10 @@
+/*
+  canon.cpp - Library for triggering the Canon EOS Rebal 
+  Created by Andrew D. Ehrlich, April 11th, 2013
+*/
+
+#include "Arduino.h"
+#include "dslr_h
+
+dslr::dslr(int pin)
+ 
---
+++ DSLRTrigger/dslr.h
@@ -0,0 +1,22 @@
+/*
+  Canon.h - Library for triggering the Canon EOS Rebal 
+  Created by Andrew D. Ehrlich, April 11th, 2013
+*/
+
+#ifndef dslr_h
+#define dslr_h
+
+#include "Arduino.h"
+
+class Canon
+{
+    public:
+		canonIR (int pin);
+		canonFocas (int pin);
+		canonShutter (int pin);
+		void dslrShoot();
+		void dslrFocas();
+	private:
+		int _pin;
+};
+#endif
