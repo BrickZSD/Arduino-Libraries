2bb251190ef312ce228b990674923e798dd2b282
Andrew <catblacka@hotmail.com>
2013-04-15 18:27:21 -0400

    Updated Code

--- DSLRTrigger/dslr.cpp
+++ DSLRTrigger/dslr.cpp
@@ -7,11 +7,34 @@
 #include "Arduino.h"
 #include "dslr_h
 
-dslr::dslr(int irpin)
+dslr::dslr(int irPin)
+    {
+        pinMode(irPin, OUTPUT);
+        _irPin = irPin;
+    }
+    
 dslr::dslr(int focusPin)
+    {
+        pinMode(focusPin);
+        _focusPin = focusPin;
+    }
+    
 dslr::dslr(int shutterPin)
-
+    {
+        pinMode(shutterPin);
+        _focusPin = focusPin;
+    }
 void dslr::dslrShoot()
+    {
+        digitalWrite(_irPin, HIGH);
+        digitalWrite(_focusPin, HIGH);
+        digitalWrite(_shutterPin, HIGH);
+    }
 
 void dslr::dslrFocas()
+    {
+        digitalWrite(_irPin, LOW);
+        digitalWrite(_focusPin, LOW);
+        digitalWrite(_shutterPin, LOW);
+    }
  
